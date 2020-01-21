57c038528f05548c2c3d39757d08013be4b75c9f
Eugene Y <yankogat@gmail.com>
2020-01-22 02:39:07 +0500

    prettify submit form

--- gate-frontend/src/component/CContestSubmitTab.js
+++ gate-frontend/src/component/CContestSubmitTab.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import apiSingleton from "../ApiSingleton";
 import Form from "react-bootstrap/Form";
 import CForm from "./CForm";
+import {Button} from "react-bootstrap";
 
 export default class CContestSubmitTab extends CForm {
     constructor(props) {
@@ -38,24 +39,26 @@ export default class CContestSubmitTab extends CForm {
 
     render() {
         return <Form onSubmit={this.handleSubmit}>
-            <select name="selectedCompiler" onChange={this.handleInputChange}>
-                <option value={undefined}>-Выберите компилятор-</option>
-                {
-                    this.state.compilerList.map(compiler =>
-                        <option value={compiler.value}>{compiler.name}</option>
-                    )
-                }
-            </select>
-            <select name="selectedProblem" onChange={this.handleInputChange}>
-                <option value={undefined}>-Выберите задачу-</option>
-                {
-                    this.state.problemList.map(problem =>
-                        <option value={problem.id}>{problem.name}</option>
-                    )
-                }
-            </select>
-            <textarea name="solutionSrc" value={this.state.solutionSrc} onChange={this.handleInputChange}/>
-            <button>Послать решение</button>
+            <Form.Control as="textarea" name="solutionSrc" value={this.state.solutionSrc} onChange={this.handleInputChange} rows="10"/>
+            <div className="submit-form-row">
+                <Form.Control size="lg" as="select" name="selectedProblem" onChange={this.handleInputChange}>
+                    <option value={undefined}>-Выберите задачу-</option>
+                    {
+                        this.state.problemList.map(problem =>
+                            <option value={problem.id}>{problem.name}</option>
+                        )
+                    }
+                </Form.Control>
+                <Form.Control size="lg" as="select" name="selectedCompiler" onChange={this.handleInputChange}>
+                    <option value={undefined}>-Выберите компилятор-</option>
+                    {
+                        this.state.compilerList.map(compiler =>
+                            <option value={compiler.value}>{compiler.name}</option>
+                        )
+                    }
+                </Form.Control>
+                <Button className="submit-form-button" size="lg" variant="primary">Послать решение</Button>
+            </div>
         </Form>
     }
 }
